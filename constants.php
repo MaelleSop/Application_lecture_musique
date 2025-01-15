41b56e5bb8f7b8f91a7850efcb89ad923dca2b29
MaelleSop <108056541+MaelleSop@users.noreply.github.com>
2025-01-15 11:38:40 -0500

    db

---
+++ constants.php
@@ -0,0 +1,8 @@
+<?php
+//Connexion bd test
+define ("DB_USER", 'postgres');
+define ("DB_PASSWORD", 'postgres');
+define ("DB_NAME", 'projet');
+define ("DB_SERVER", '127.0.0.1');
+define ("DB_PORT", '5432');
+?>
